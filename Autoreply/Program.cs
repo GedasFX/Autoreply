bb871b85e6652d4bb337ef3a4c6956763e5f6d36
Gediminas Milašius <gediminasmilasius@gmail.com>
2020-10-03 13:41:48 +0300

    init

---
+++ Autoreply/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Autoreply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
